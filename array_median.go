baa6d7ba0ee55c1102fb0c9ab434d7ca8aa5a69c
Oleg Kubrakov <oleg.kubrakov@bgl.hk>
2019-09-15 14:39:57 +0800

    find a median of two sorted arrays: a+b.
    There are two sorted arrays nums1 and nums2 of size m and n respectively.
    Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
    
    Solution draft.

---
+++ array_median.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"log"
+	"math"
+)
+
+type TestData struct {
+	A      []int
+	B      []int
+	Result float64
+}
+
+func main() {
+	testData := []TestData{
+		TestData{A: []int{1, 2}, B: []int{}, Result: float64(1.5)},
+		TestData{A: []int{1, 2, 3}, B: []int{}, Result: float64(2)},
+		TestData{A: []int{1, 2}, B: []int{3, 4}, Result: float64(2.5)},
+		TestData{A: []int{3, 4}, B: []int{1, 2}, Result: float64(2.5)},
+		TestData{A: []int{1, 4, 6}, B: []int{3, 5}, Result: float64(4)},
+		TestData{A: []int{1, 5, 9, 12}, B: []int{2, 4, 7, 8, 10, 15}, Result: float64(7.5)},
+		TestData{A: []int{2, 4, 7, 8, 10, 15}, B: []int{1, 5, 9, 12}, Result: float64(7.5)},
+		TestData{A: []int{1, 3}, B: []int{2}, Result: float64(2)},
+		TestData{A: []int{3}, B: []int{-2, -1}, Result: float64(-1)},
+		TestData{A: []int{2, 2, 2}, B: []int{2, 2, 2, 2}, Result: float64(2)},
+		TestData{A: []int{1}, B: []int{2, 3}, Result: float64(2)},
+		TestData{A: []int{1}, B: []int{2, 3, 4}, Result: float64(2.5)},
+		TestData{A: []int{1}, B: []int{2, 3, 4, 5}, Result: float64(3)},
+		TestData{A: []int{2, 3, 4}, B: []int{1}, Result: float64(2.5)},
+		TestData{A: []int{1}, B: []int{-3, -2, -1}, Result: float64(-1.5)},
+		TestData{A: []int{10}, B: []int{1, 2, 3, 4}, Result: float64(3)},
+		TestData{A: []int{10}, B: []int{1, 2, 3}, Result: float64(2.5)},
+	}
+	for _, testSet := range testData {
+		if r := findMedianSortedArrays(testSet.A, testSet.B); r != testSet.Result {
+			log.Fatalln("Incorrect result: ", r)
+		} else {
+			log.Println("Correct result: ", r)
+		}
+	}
+	log.Println("Finish")
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	log.Println(nums1)
+	log.Println(nums2)
+
+	if len(nums1) == 0 {
+		return arrayMedian(nums2)
+	} else if len(nums2) == 0 {
+		return arrayMedian(nums1)
+	}
+
+	// what diff in slices do we need before we are ready for median
+	f := 1
+	if math.Mod(float64(len(nums1)+len(nums2)), 2) == 0 {
+		f = 0
+	}
+	log.Println(f)
+
+	i := centerPosition(nums1)
+	j := centerPosition(nums2)
+
+	// then we split to nums1[0:i-1] + nums2[0:j-1] as the left and rest as the right
+	// and start to joggling elements left or right until we get parity
+	for {
+		log.Println("i=", i, "j=", j)
+		// median will be in the left half
+		if i+j+f == len(nums1)-i+len(nums2)-j {
+			if f == 0 {
+				var left int
+				var right int
+				// how nums1 and nums2 contribute to left side of the merged array
+				if j > 0 && i > 0 {
+					left = max(nums1[i-1], nums2[j-1])
+					if i == len(nums1) {
+						right = nums2[j]
+					} else if j == len(nums2) {
+						right = nums1[i]
+					} else {
+						right = min(nums1[i], nums2[j])
+					}
+				} else if j > 0 && i == 0 {
+					left = nums2[j-1]
+					if j == len(nums2) {
+						right = nums1[0]
+					} else {
+						right = min(nums2[j], nums1[0])
+					}
+				} else if j == 0 && i > 0 {
+					left = nums1[i-1]
+					right = nums2[0]
+				} else {
+					panic("error!")
+				}
+
+				return float64(left+right) / 2
+			}
+			if i == len(nums1) && j < len(nums2) {
+				return float64(nums2[j])
+			} else if j == len(nums2) && i < len(nums1) {
+				return float64(nums1[i])
+			}
+			return float64(min(nums1[i], nums2[j])) // because sorted in ascending order
+		}
+		if i == len(nums1) || nums2[j] < nums1[i] {
+			j++
+		} else {
+			i++
+		}
+	}
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func arrayMedian(arr []int) float64 {
+	if len(arr)%2 == 0 {
+		return float64(arr[int(len(arr)/2)-1]+arr[int(len(arr)/2)]) / 2
+	}
+	return float64(arr[int(len(arr)/2)])
+}
+
+// After finding the center of nums1, we need to find where this central element could be placed in nums2
+func findPositionOfElementInSortedArray(a int, nums []int) int {
+	j := centerPosition(nums)
+	if j == len(nums) {
+		j--
+	}
+	for {
+		if j == 0 || j == len(nums)-1 {
+			return j
+		}
+		if a < nums[j] && a > nums[j-1] {
+			return j
+		}
+		if a < nums[j] {
+			j--
+		} else if a > nums[j] {
+			j++
+		} else { // a == nums[j]
+			return j
+		}
+	}
+}
+
+func centerPosition(nums []int) int {
+	return int(math.Round(float64(len(nums))/2)) - 1
+}
