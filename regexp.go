7ee4f9be9327d2e1e716aaf54f23d12f7ba3dbb2
Oleg Kubrakov <oleg.kubrakov@bgl.hk>
2019-10-20 19:36:15 +0800

    149. Max Points on a Line

--- regexp.go
+++ regexp.go
@@ -5,7 +5,13 @@ import (
 )
 
 func main() {
-	log.Println(longestValidParentheses(")))()())"))
+	// [[1,1],[1,1],[2,3]]
+	log.Println(maxPoints([][]int{[]int{5, 1}, []int{1, 1}, []int{1, 1}, []int{2, 3}, []int{2, 3}, []int{1, 1}, []int{3, 5}}))
+
+	log.Println(maxPoints([][]int{[]int{0, 0}, []int{0, 0}}))
+
+	//[[0,0],[94911151,94911150],[94911152,94911151]]
+	log.Println(maxPoints([][]int{[]int{0, 0}, []int{94911151, 94911150}, []int{94911152, 94911151}}))
 }
 
 func isMatch(s string, p string) bool {
@@ -83,4 +89,53 @@ func longestValidParentheses(s string) int {
 		i++
 	}
 	return max
+}
+
+// 149. Max Points on a Line
+// Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+func maxPoints(points [][]int) int {
+	i := 0
+	max := 0
+
+	if len(points) == 1 {
+		return 1
+	}
+	for i < len(points)-1 {
+		duplicates := 0
+		j := i + 1
+		for j < len(points) {
+			if points[i][0] == points[j][0] && points[i][1] == points[j][1] {
+				duplicates++
+				if max < duplicates+1 {
+					max = duplicates + 1
+				}
+			} else {
+				s := slope(points[i], points[j])
+				log.Println(i, j, s)
+				l := j + 1
+				counter := 2 + duplicates
+				for l < len(points) {
+					if slope(points[l], points[j]) == s || (points[l][0] == points[j][0] && points[l][1] == points[j][1]) {
+						counter++
+					}
+					log.Println("l", slope(points[l], points[j]), s, l, counter)
+					l++
+				}
+				if max < counter {
+					max = counter
+				}
+			}
+			j++
+		}
+		i++
+	}
+	return max
+}
+
+func slope(p1 []int, p2 []int) float64 {
+	slope := float64(p1[1]-p2[1]) / float64(p1[0]-p2[0])
+	if p1[0]-p2[0] == 0 {
+		slope = 0
+	}
+	return slope
 }
