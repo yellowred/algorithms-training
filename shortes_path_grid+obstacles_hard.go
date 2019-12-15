5cec3252bd31a33b5691b25fb22d09547a993ab9
Oleg <oleg.kubrakov@bgl.hk>
2019-12-15 22:27:25 +0800

    explore all paths with obstacles elemination

--- shortes_path_grid+obstacles_hard.go
+++ shortes_path_grid+obstacles_hard.go
@@ -5,12 +5,19 @@ package main
 
 import (
 	"log"
+	"math"
 )
 
 func main() {
 	//log.Println(shortestPath([][]int{[]int{0, 0, 0}, []int{1, 1, 0}, []int{0, 0, 0}, []int{0, 1, 1}, []int{0, 0, 0}}, 1))
 	//log.Println(shortestPath([][]int{[]int{0, 0, 0}, []int{1, 1, 0}, []int{0, 0, 0}}, 1))
 	log.Println(shortestPath([][]int{[]int{0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1}, []int{0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1}, []int{1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0}, []int{1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1}, []int{1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1}, []int{0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1}, []int{0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1}, []int{1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0}, []int{0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0}, []int{0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0}, []int{0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0}}, 27))
+	// log.Println(shortestPath(
+	// 	[][]int{
+	// 		[]int{0, 1, 0, 0},
+	// 		[]int{0, 0, 0, 1},
+	// 		[]int{0, 1, 1, 0},
+	// 	}, 1))
 }
 
 // Lee Algorithm: http://users.eecs.northwestern.edu/~haizhou/357/lec6.pdf
@@ -24,46 +31,54 @@ func shortestPath(grid [][]int, k int) int {
 		return -1
 	}
 
-	visited := make([][]bool, m)
+	visited := make([][][]int, m)
 	for i := 0; i < m; i++ {
-		visited[i] = make([]bool, n)
+		visited[i] = make([][]int, n)
+		for j := 0; j < n; j++ {
+			visited[i][j] = make([]int, k+1)
+			for l := 0; l <= k; l++ {
+				visited[i][j][l] = math.MaxInt32
+			}
+		}
 	}
-	visited[0][0] = true
+	visited[0][0][k] = 0
+	// visited[m-1][n-1][k] = -1
 
-	queue := [][]int{} //0 - x, 1 - y, 2 - dst, 3 - k
-	queue = append(queue, []int{0, 0, 0, 0})
-
+	queue := [][]int{} //0 - x, 1 - y, 2 - k
+	queue = append(queue, []int{0, 0, k})
+	// log.Println(queue)
 	for len(queue) > 0 {
 		queueNode := queue[0]
-
-		// If we have reached the destination cell,
-		// we are done
-		if queueNode[0] == m-1 && queueNode[1] == n-1 {
-			return queueNode[2]
-		}
-
-		// Otherwise dequeue the front cell in the queue
-		// and enqueue its adjacent cells
 		queue = queue[1:len(queue)]
-
+		// log.Println("ITER", queueNode, visited[queueNode[0]][queueNode[1]][queueNode[2]])
 		for i := 0; i < 4; i++ {
+			x := queueNode[0]
+			y := queueNode[1]
 			row := queueNode[0] + rowNum[i]
 			col := queueNode[1] + colNum[i]
 
+			// log.Println("Queue=", queue)
+			// log.Println("Visited=", visited)
 			// if adjacent cell is valid, has path and
 			// not visited yet, enqueue it.
-			if row >= 0 && col >= 0 && row < m && col < n && !visited[row][col] {
-				if grid[row][col] == 0 {
-					// mark cell as visited and enqueue it
-					visited[row][col] = true
-					queue = append(queue, []int{row, col, queueNode[2] + 1, queueNode[3]})
-				} else if queueNode[3] < k {
-					// mark cell as visited and enqueue it
-					visited[row][col] = true
-					queue = append(queue, []int{row, col, queueNode[2] + 1, queueNode[3] + 1})
+			if row >= 0 && col >= 0 && row < m && col < n {
+				nCost := queueNode[2] - grid[row][col]
+
+				if nCost >= 0 && visited[row][col][nCost] > visited[x][y][queueNode[2]]+1 {
+					visited[row][col][nCost] = visited[x][y][queueNode[2]] + 1
+					queue = append(queue, []int{row, col, nCost})
 				}
 			}
 		}
 	}
-	return -1
+	ans := -1
+	for i := 0; i <= k; i++ {
+		c := visited[m-1][n-1][i]
+		if c != math.MaxInt32 {
+			if ans == -1 || c < ans {
+				ans = c
+			}
+		}
+	}
+	return ans
 }
