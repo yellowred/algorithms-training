7c06d676e6a42fcd7c6c3b80d00649f99d0d4e3d
Oleg <oleg.kubrakov@bgl.hk>
2019-12-29 12:37:19 +0800

    fist iter 5298. Verbal Arithmetic Puzzle

--- exec.go
+++ exec.go
@@ -7,65 +7,107 @@ package main
 
 import (
 	"log"
+	"strconv"
 )
 
 func main() {
-	nums := []int{4, 5, 3, 1, 2}
-	queue := []int{}
-	for k := range nums {
-		PushToQueue(&queue, nums[k])
-	}
-	log.Println(queue)
-	for i := 0; i < 5; i++ {
-		log.Println(PopFromQueue(&queue))
-	}
+	// nums := []int{4, 5, 3, 1, 2}
+	log.Println(isSolvable([]string{"SEND", "MORE"}, "MONEY"))
+	// log.Println(isSolvable([]string{"SE", "ME"}, "GE"))
 }
 
-func PushToQueue(queue *[]int, x int) {
-	*queue = append(*queue, x)
-	j := len(*queue) - 1
-	for {
-		i := (j - 1) / 2 // parent
-		if i == j || (*queue)[j] < (*queue)[i] {
-			break
+var wordEvalDic111 map[string]int
+
+func isSolvable(words []string, result string) bool {
+	charMap := map[byte]uint{}
+	charOrder := []byte{}
+	firstDigits := map[byte]bool{}
+	wordEvalDic111 = map[string]int{}
+	makeCharMap(append(words, result), &charMap, &charOrder, &firstDigits)
+	log.Println(charMap)
+	res := true
+	for res {
+		if evaluate(words, result, charMap) {
+			printMap(charMap)
+			return true
 		}
-		(*queue)[j], (*queue)[i] = (*queue)[i], (*queue)[j]
-		j = i
+		res = plusOne(&charMap, charOrder, firstDigits)
+	}
+	return res
+}
+
+func makeCharMap(arr []string, charMap *map[byte]uint, charOrder *[]byte, fd *map[byte]bool) {
+	idx := uint(0)
+	zeroCan := false
+	for _, w := range arr {
+		for i := 0; i < len(w); i++ {
+			if _, ok := (*charMap)[w[i]]; ok {
+				// do nothing
+			} else {
+				if i == 0 {
+					if idx == 0 {
+						idx++
+						zeroCan = true
+					}
+					(*charMap)[w[i]] = idx // no leading zeroes
+					(*fd)[w[i]] = true
+				} else {
+					if zeroCan {
+						(*charMap)[w[i]] = 0
+					} else {
+						(*charMap)[w[i]] = idx
+					}
+
+					(*fd)[w[i]] = false
+				}
+				*charOrder = append(*charOrder, w[i])
+			}
+		}
 	}
 }
 
-// 1. Replace the root node with the last element in the heap
-// 2. Remove the last element
-// 3. Swap (i.e. heapify) the new root with its child until the correct position has found (See MAX-HEAPIFY)
-func PopFromQueue(queue *[]int) int {
-	x := (*queue)[0]
-	(*queue)[0] = (*queue)[len(*queue)-1]
-	(*queue) = (*queue)[0 : len(*queue)-1]
-	heapify(*queue)
-	log.Println(*queue)
-	return x
-}
+func plusOne(charMap *map[byte]uint, charOrder []byte, fd map[byte]bool) bool {
 
-func heapify(queue []int) {
-	i := 0
-	n := len(queue)
-	g := i
-	for {
-		l := i*2 + 1
-		r := i*2 + 2
-		if l < n && queue[i] < queue[l] {
-			g = l
+	i := len(charOrder) - 1
+	for i >= 0 {
+		(*charMap)[charOrder[i]]++
+		if (*charMap)[charOrder[i]] > 9 { // carry
+			if fd[charOrder[i]] {
+				(*charMap)[charOrder[i]] = 1
+			} else {
+				(*charMap)[charOrder[i]] = 0
+			}
+			i--
 		} else {
-			g = i
+			return true
 		}
-		if r < n && queue[g] < queue[r] {
-			g = r
+	}
+	return false
+}
+
+func evaluate(arr []string, res string, charMap map[byte]uint) bool {
+	sum := 0
+	for _, w := range arr {
+		wc := ""
+		for i := 0; i < len(w); i++ {
+			wc = wc + strconv.Itoa(int(charMap[w[i]]))
 		}
-		if g != i {
-			queue[i], queue[g] = queue[g], queue[i]
-			i = g
-		} else {
-			break
-		}
+		intInt, _ := strconv.Atoi(wc)
+		sum = sum + intInt
+	}
+	wc := ""
+	for i := 0; i < len(res); i++ {
+		wc = wc + strconv.Itoa(int(charMap[res[i]]))
+	}
+	intInt, _ := strconv.Atoi(wc)
+	if sum == intInt {
+		return true
+	}
+	return false
+}
+
+func printMap(chm map[byte]uint) {
+	for k := range chm {
+		log.Println(string(k), chm[k])
 	}
 }
